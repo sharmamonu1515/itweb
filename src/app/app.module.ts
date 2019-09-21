15288bf1e8a3d11344de9c1ef9ff6913884f6c96
sharmamonu1515 <sharmamonu1515@gmail.com>
2019-09-21 12:33:13 +0530

    adding intial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
